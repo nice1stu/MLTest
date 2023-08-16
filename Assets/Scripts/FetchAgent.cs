828ec0412dfa9789523e06f4acb1e2e65bc00917
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-08-17 00:57:49 +0200

    Set up Unity environment for MLAgent testing

---
+++ Assets/Scripts/FetchAgent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class FetchAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+    }
+}
