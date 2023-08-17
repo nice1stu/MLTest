27dc30c392b85c1dfb54d757cdd87a1f570ad4e8
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-08-17 03:10:04 +0200

    Target is random spawned

--- Assets/Scripts/FetchAgent.cs
+++ Assets/Scripts/FetchAgent.cs
@@ -1,21 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using Random = UnityEngine.Random;
 
 public class FetchAgent : Agent
 {
     [SerializeField] private Transform targetTransform;
     [SerializeField] private Material winMaterial;
     [SerializeField] private Material loseMaterial;
-    [SerializeField] private MeshRenderer FloorMeshRenderer;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
 
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = new Vector3(-6f, 0.5f, -4.5f);
+        transform.localPosition = new Vector3(-6f, 0.5f, 0f);
+        targetTransform.localPosition = new Vector3(Random.Range(-1.5f, -10.5f), 0.5f, Random.Range(-4.5f, 4.5f));
     }
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -44,13 +44,13 @@ public class FetchAgent : Agent
         if (other.TryGetComponent<TagTarget>(out TagTarget tagTarget))
         {
             SetReward(1.0f);
-            FloorMeshRenderer.material = winMaterial;
+            floorMeshRenderer.material = winMaterial;
             EndEpisode();
         }
         if (other.TryGetComponent<TagWall>(out TagWall tagWall))
         {
             SetReward(-1.0f);
-            FloorMeshRenderer.material = loseMaterial;
+            floorMeshRenderer.material = loseMaterial;
             EndEpisode();
         }
     }
