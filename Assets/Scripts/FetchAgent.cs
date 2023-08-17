bf384cdf50a2c485d513e560e8c51113ca2ba274
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-08-17 02:03:10 +0200

    Have movement and colliders set up

--- Assets/Scripts/FetchAgent.cs
+++ Assets/Scripts/FetchAgent.cs
@@ -1,13 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class FetchAgent : Agent
 {
+    [SerializeField] private Transform targetTransform;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(0f, 1.0f, -4.5f);
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+
     public override void OnActionReceived(ActionBuffers actions)
     {
-        base.OnActionReceived(actions);
+        float moveZ = actions.ContinuousActions[0];
+        float moveX = -actions.ContinuousActions[1];
+
+        float moveSpeed = 3.0f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<TagTarget>(out TagTarget tagTarget))
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        if (other.TryGetComponent<TagWall>(out TagWall tagWall))
+        {
+            SetReward(-1.0f);
+            EndEpisode();
+        }
     }
 }
